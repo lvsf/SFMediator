5eb4a228e72ea9c0c69c21bab91145f07d27a9d2
YunSL <yunsl16@163.com>
2017-10-17 11:19:49 +0800

    fix

--- SFMediator/Class/SFMediator.h
+++ SFMediator/Class/SFMediator.h
@@ -11,7 +11,7 @@
 
 /**
  快速声明一个用来转发协议的类
-
+ 
  @param name 协议定义的名称前缀 例如 Book(协议名称前缀) + ForwardTarget(固定名称)
  @return
  */
@@ -26,6 +26,7 @@ _Pragma("clang diagnostic ignored \"-Wprotocol\"")\
 
 @interface SFMediatorURLInvokeComponent : NSObject
 @property (nonatomic,assign) SEL selector;
+@property (nonatomic,copy) NSString *scheme;
 @property (nonatomic,copy) NSString *protocolName;
 @property (nonatomic,copy) NSDictionary *parameters;
 @property (nonatomic,copy) NSArray *parameterValues;
@@ -34,23 +35,30 @@ _Pragma("clang diagnostic ignored \"-Wprotocol\"")\
 
 @protocol SFMediatorProtocolParser <NSObject>
 /**
+ 可远程响应的URL协议名
+ 
+ @return URL协议名
+ */
+- (NSString*)targetURLSchemeForInvoke;
+/**
  获取指定协议对应的调用对象,默认为协议名称 + Target
-
+ 
  @param protocol 指定的协议
  @return 协议调用对象
  */
 - (id)targetFromProtocol:(Protocol*)protocol;
 /**
  获取从指定URL中解析得到的调用协议名称,方法的名字以及传入的参数
-
- @param url 指定的URL
+ 
+ @param URL 指定的URL
  @return URL解析结果
  */
-- (SFMediatorURLInvokeComponent*)targetInvokeComponentFromURL:(NSString*)url;
+- (SFMediatorURLInvokeComponent*)targetInvokeComponentFromURL:(NSURL*)URL;
 @end
 
 @interface SFMediator : NSObject
 @property (nonatomic,strong) id <SFMediatorProtocolParser> parser;
++ (BOOL)canInvokeURL:(NSString*)url;
 + (id)invokeURL:(NSString*)url;
 + (id)invokeTargetWithProtocol:(Protocol*)protocol
                  forwardTarget:(id)forwardTarget;
--- SFMediator/Class/SFMediator.m
+++ SFMediator/Class/SFMediator.m
@@ -9,6 +9,12 @@
 #import "SFMediator.h"
 #import <objc/runtime.h>
 
+static inline NSURL* SFURLPraser(NSString *url) {
+    NSString *encodeURL = [url stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+    NSURL *URL = [NSURL URLWithString:encodeURL];
+    return URL;
+}
+
 @implementation SFMediatorURLInvokeComponent @end
 
 @interface SFMediatorDefaultParser : NSObject<SFMediatorProtocolParser>
@@ -16,47 +22,44 @@
 
 @implementation SFMediatorDefaultParser
 
-- (SFMediatorURLInvokeComponent *)targetInvokeComponentFromURL:(NSString *)url {
-    NSString *encodeURL = [url stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
-    NSURL *URL = [NSURL URLWithString:encodeURL];
-    SFMediatorURLInvokeComponent *component = nil;
-    if (URL) {
-        if (URL.scheme.length > 0) {
-            if (URL.host.length > 0) {
-                component = [SFMediatorURLInvokeComponent new];
-                component.protocolName = [URL.host stringByAppendingString:@"Protocol"];
-                component.selector = NSSelectorFromString([URL.path stringByReplacingOccurrencesOfString:@"/" withString:@""]);
-                component.parameters = ({
-                    NSMutableDictionary *params = [NSMutableDictionary new];
-                    NSMutableArray *values = [NSMutableArray new];
-                    NSString *query = URL.query;
-                    if (query.length > 0) {
-                        NSArray *components = [query componentsSeparatedByString:@"&"];
-                        [components enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-                            NSArray *elements = [obj componentsSeparatedByString:@"="];
-                            if (elements.count == 2) {
-                                id value = elements.lastObject;
-                                if ([value isKindOfClass:[NSString class]]) {
-                                    NSString *string = (NSString*)value;
-                                    if (string.length == 0) {
-                                        value = [NSNull null];
-                                    }
-                                    else if ([string rangeOfString:@"demo"].location != NSNotFound) {
-                                        value = [SFMediator invokeURL:string];
-                                    }
-                                }
-                                [params setObject:value
-                                           forKey:elements.firstObject];
-                                [values addObject:value];
-                            }
-                        }];
+- (NSString *)targetURLSchemeForInvoke {
+    return @"demo";
+}
+
+- (SFMediatorURLInvokeComponent *)targetInvokeComponentFromURL:(NSURL *)URL {
+    SFMediatorURLInvokeComponent *component = [SFMediatorURLInvokeComponent new];
+    component.scheme = URL.scheme;
+    component.protocolName = [URL.host stringByAppendingString:@"Protocol"];
+    component.selector = NSSelectorFromString([URL.path stringByReplacingOccurrencesOfString:@"/" withString:@""]);
+    component.parameters = ({
+        NSMutableDictionary *params = [NSMutableDictionary new];
+        NSMutableArray *values = [NSMutableArray new];
+        NSString *query = URL.query;
+        if (query.length > 0) {
+            NSArray *components = [query componentsSeparatedByString:@"&"];
+            [components enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                NSArray *elements = [obj componentsSeparatedByString:@"="];
+                if (elements.count == 2) {
+                    id value = elements.lastObject;
+                    if ([value isKindOfClass:[NSString class]]) {
+                        NSString *string = (NSString*)value;
+                        if (string.length == 0) {
+                            value = [NSNull null];
+                        }
+                        else if ([SFMediator canInvokeURL:string]) {
+                            value = [SFMediator invokeURL:string];
+                            value = value?:[NSNull null];
+                        }
                     }
-                    component.parameterValues = values.copy;
-                    params;
-                });
-            }
+                    [params setObject:value
+                               forKey:elements.firstObject];
+                    [values addObject:value];
+                }
+            }];
         }
-    }
+        component.parameterValues = values.copy;
+        params;
+    });
     return component;
 }
 
@@ -120,84 +123,102 @@
     return instance;
 }
 
-+ (id)invokeURL:(NSString *)url {
-    id returnValue = nil;
++ (BOOL)canInvokeURL:(NSString *)url {
     SFMediator *manager = [self sharedInstance];
-    SFMediatorURLInvokeComponent *component = [manager.parser targetInvokeComponentFromURL:url];
-    SFMediatorItem *mediatorItem = [self invokeTargetWithProtocol:objc_getProtocol(component.protocolName.UTF8String) forwardTarget:component.forwardTarget];
-    NSMethodSignature *signature = [mediatorItem methodSignatureForSelector:component.selector];
-    if (mediatorItem && signature) {
-        NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
-        [invocation setTarget:mediatorItem];
-        [invocation setSelector:component.selector];
-        [component.parameterValues enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            //此处应该封装一个协议用来处理URL获取的参数类型到OC类型的转换
-            const char *argumentType = [invocation.methodSignature getArgumentTypeAtIndex:idx + 2];
-            
-            if (strcmp(argumentType, @encode(NSInteger)) == 0) {
-                NSInteger argument = [obj integerValue];
-                [invocation setArgument:&argument atIndex:idx + 2];
+    NSURL *URL = SFURLPraser(url);
+    BOOL can = NO;
+    if (URL) {
+        if ([URL.scheme isEqualToString:[manager.parser targetURLSchemeForInvoke]]) {
+            if (URL.path.length > 0) {
+                can = YES;
             }
-            else if (strcmp(argumentType, @encode(float)) == 0) {
-                float argument = [obj floatValue];
-                [invocation setArgument:&argument atIndex:idx + 2];
-            }
-            else if (strcmp(argumentType, @encode(double)) == 0) {
-                double argument = [obj doubleValue];
-                [invocation setArgument:&argument atIndex:idx + 2];
-            }
-            else if (strcmp(argumentType, @encode(BOOL)) == 0) {
-                BOOL argument = [obj boolValue];
-                [invocation setArgument:&argument atIndex:idx + 2];
-            }
-            else {
-                id argument = obj;
-                [invocation setArgument:&argument atIndex:idx + 2];
-            }
-        }];
-        //暂时处理返回为void,BOOL,NSInteger,CGFloat,CGSize,CGRect,CGPoint的调用,其他视为返回NSObject对象处理
-        const char *returnType = [invocation.methodSignature methodReturnType];
-        if (strcmp(returnType, @encode(void)) == 0) {
-            [invocation invoke];
-            returnValue = nil;
-        } else if (strcmp(returnType, @encode(BOOL)) == 0) {
-            BOOL result = NO;
-            [invocation invoke];
-            [invocation getReturnValue:&result];
-            returnValue = @(result);
-        } else if (strcmp(returnType, @encode(NSInteger)) == 0) {
-            NSInteger result = 0;
-            [invocation invoke];
-            [invocation getReturnValue:&result];
-            returnValue = @(result);
-        } else if (strcmp(returnType, @encode(CGFloat)) == 0) {
-            CGFloat result = 0.0;
-            [invocation invoke];
-            [invocation getReturnValue:&result];
-            returnValue = @(result);
-        } else if (strcmp(returnType, @encode(CGSize)) == 0) {
-            CGSize result = CGSizeZero;
-            [invocation invoke];
-            [invocation getReturnValue:&result];
-            returnValue = [NSValue valueWithCGSize:result];
-        } else if (strcmp(returnType, @encode(CGRect)) == 0) {
-            CGRect result = CGRectZero;
-            [invocation invoke];
-            [invocation getReturnValue:&result];
-            returnValue = [NSValue valueWithCGRect:result];
-        } else if (strcmp(returnType, @encode(CGPoint)) == 0) {
-            CGPoint result = CGPointZero;
-            [invocation invoke];
-            [invocation getReturnValue:&result];
-            returnValue = [NSValue valueWithCGPoint:result];
-        } else {
-            __autoreleasing NSObject *result = nil;
-            [invocation invoke];
-            [invocation getReturnValue:&result];
-            returnValue = result;
         }
     }
-    return returnValue;
+    return can;
+}
+
++ (id)invokeURL:(NSString *)url {
+    if ([self canInvokeURL:url]) {
+        id returnValue = nil;
+        SFMediator *manager = [self sharedInstance];
+        SFMediatorURLInvokeComponent *component = [manager.parser targetInvokeComponentFromURL:SFURLPraser(url)];
+        SFMediatorItem *mediatorItem = [self invokeTargetWithProtocol:objc_getProtocol(component.protocolName.UTF8String) forwardTarget:component.forwardTarget];
+        NSMethodSignature *signature = [mediatorItem methodSignatureForSelector:component.selector];
+        if (mediatorItem && signature) {
+            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+            [invocation setTarget:mediatorItem];
+            [invocation setSelector:component.selector];
+            [component.parameterValues enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                //此处应该封装一个协议用来处理URL获取的参数类型到OC类型的转换
+                const char *argumentType = [invocation.methodSignature getArgumentTypeAtIndex:idx + 2];
+                if (strcmp(argumentType, @encode(NSInteger)) == 0) {
+                    NSInteger argument = [obj integerValue];
+                    [invocation setArgument:&argument atIndex:idx + 2];
+                }
+                else if (strcmp(argumentType, @encode(float)) == 0) {
+                    float argument = [obj floatValue];
+                    [invocation setArgument:&argument atIndex:idx + 2];
+                }
+                else if (strcmp(argumentType, @encode(double)) == 0) {
+                    double argument = [obj doubleValue];
+                    [invocation setArgument:&argument atIndex:idx + 2];
+                }
+                else if (strcmp(argumentType, @encode(BOOL)) == 0) {
+                    BOOL argument = [obj boolValue];
+                    [invocation setArgument:&argument atIndex:idx + 2];
+                }
+                else {
+                    id argument = obj;
+                    [invocation setArgument:&argument atIndex:idx + 2];
+                }
+            }];
+            //暂时处理返回为void,BOOL,NSInteger,CGFloat,CGSize,CGRect,CGPoint的调用,其他视为返回NSObject对象处理
+            const char *returnType = [invocation.methodSignature methodReturnType];
+            if (strcmp(returnType, @encode(void)) == 0) {
+                [invocation invoke];
+                returnValue = nil;
+            } else if (strcmp(returnType, @encode(BOOL)) == 0) {
+                BOOL result = NO;
+                [invocation invoke];
+                [invocation getReturnValue:&result];
+                returnValue = @(result);
+            } else if (strcmp(returnType, @encode(NSInteger)) == 0) {
+                NSInteger result = 0;
+                [invocation invoke];
+                [invocation getReturnValue:&result];
+                returnValue = @(result);
+            } else if (strcmp(returnType, @encode(CGFloat)) == 0) {
+                CGFloat result = 0.0;
+                [invocation invoke];
+                [invocation getReturnValue:&result];
+                returnValue = @(result);
+            } else if (strcmp(returnType, @encode(CGSize)) == 0) {
+                CGSize result = CGSizeZero;
+                [invocation invoke];
+                [invocation getReturnValue:&result];
+                returnValue = [NSValue valueWithCGSize:result];
+            } else if (strcmp(returnType, @encode(CGRect)) == 0) {
+                CGRect result = CGRectZero;
+                [invocation invoke];
+                [invocation getReturnValue:&result];
+                returnValue = [NSValue valueWithCGRect:result];
+            } else if (strcmp(returnType, @encode(CGPoint)) == 0) {
+                CGPoint result = CGPointZero;
+                [invocation invoke];
+                [invocation getReturnValue:&result];
+                returnValue = [NSValue valueWithCGPoint:result];
+            } else {
+                __autoreleasing NSObject *result = nil;
+                [invocation invoke];
+                [invocation getReturnValue:&result];
+                returnValue = result;
+            }
+        }
+        return returnValue;
+    }
+    else {
+        return nil;
+    }
 }
 
 + (id)invokeTargetWithProtocol:(Protocol *)protocol forwardTarget:(id)forwardTarget {
