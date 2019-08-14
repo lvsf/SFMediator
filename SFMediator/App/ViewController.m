54a0e1d33a04dee6ad0e511baa592f3c7372a167
lvsf <lvsf1992@163.com>
2019-08-14 10:15:41 +0800

    update

--- SFMediator/App/ViewController.m
+++ SFMediator/App/ViewController.m
@@ -8,29 +8,99 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface XLayout : UICollectionViewFlowLayout
+
+@end
+
+@implementation XLayout
+
+- (void)prepareLayout {
+    [super prepareLayout];
+}
+
+- (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds {
+    return true;
+}
+
+//- (NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect {
+//    NSArray<UICollectionViewLayoutAttributes *> *a = [super layoutAttributesForElementsInRect:rect];
+//    [a enumerateObjectsUsingBlock:^(UICollectionViewLayoutAttributes * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//        CGRect frame = obj.frame;
+//        frame.origin.x = obj.indexPath.row * self.collectionView.bounds.size.width +  (self.collectionView.bounds.size.width - frame.size.width) * 0.5;
+//        obj.frame = frame;
+//    }];
+//
+//    NSLog(@"%@",NSStringFromCGRect(rect));
+//
+//    return a;
+//}
+
+- (CGPoint )targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity
+{
+    CGFloat offsetAdjustment = MAXFLOAT;
+    CGFloat horizontalCenter_X = proposedContentOffset.x + CGRectGetWidth(self.collectionView.bounds)/2.0;
+    CGRect targetRect = CGRectMake(proposedContentOffset.x, 0, self.collectionView.bounds.size.width, self.collectionView.bounds.size.height);
+    NSArray *array = [super layoutAttributesForElementsInRect:targetRect];
+    for (UICollectionViewLayoutAttributes * attributes in array)
+    {
+        CGFloat itemHorizontalCenter_X = attributes.center.x;
+        if (ABS(itemHorizontalCenter_X - horizontalCenter_X) < ABS(offsetAdjustment))
+        {
+            offsetAdjustment = itemHorizontalCenter_X - horizontalCenter_X;
+        }
+    }
+    return CGPointMake(proposedContentOffset.x + offsetAdjustment, proposedContentOffset.y);
+}
+
+@end
+
+
+@interface ViewController ()<UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout>
 @property (nonatomic,strong) UILabel *label;
+@property (nonatomic,strong) UICollectionView *cv;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    self.label = [UILabel new];
-    self.label.text = @"123";
-    self.label.font = [UIFont boldSystemFontOfSize:30];
-    self.label.textAlignment = NSTextAlignmentCenter;
-    self.label.textColor = [UIColor greenColor];
-    
-    [self.view addSubview:self.label];
-    
-    [self.view setBackgroundColor:[UIColor lightGrayColor]];
 }
 
 - (void)viewDidLayoutSubviews {
     [super viewDidLayoutSubviews];
-    [self.label setFrame:self.view.bounds];
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"123" forIndexPath:indexPath];
+    cell.contentView.backgroundColor = [UIColor lightGrayColor];
+    return cell;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake(collectionView.bounds.size.width * 0.9, collectionView.bounds.size.height * 0.5);
+}
+
+- (UICollectionView *)cv {
+    return _cv?:({
+        _cv = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:({
+            XLayout *layout = [XLayout new];
+            layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+            layout;
+        })];;
+        _cv.backgroundColor = [UIColor whiteColor];
+        _cv.delegate = self;
+        _cv.dataSource = self;
+        _cv;
+    });
 }
 
 @end
